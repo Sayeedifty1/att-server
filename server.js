b931f5b99ecbd15bb6580f21eee500d28f4056bf
Sayeedifty1 <md.abusayeedifty@gmail.com>
2023-12-18 13:39:53 +0600

    gg

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
+
 
 // ! Middleware
 app.use(cors());
@@ -368,6 +369,6 @@ app.get('/', (req, res) => {
     res.send('Att. server is running')
 })
 
-app.listen(port, () => {
-    console.log(`server is running on port ${port}`)
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
