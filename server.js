5352d973ca019a15fa50c82b232da6a1a0363d2d
Sayeedifty1 <md.abusayeedifty@gmail.com>
2023-12-18 13:13:22 +0600

    deploy

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 // ! MongoDB Connection
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nsyuaxc.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2ionwzy.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
